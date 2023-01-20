5b5803cb06a0d3abfc8219970e4513d5bab76741
konstantin <mom.konstantin@gmail.com>
2023-01-21 02:11:34 +0300

    Сделано ДЗ Задача 1-10

--- src/Main.java
+++ src/Main.java
@@ -13,52 +13,56 @@ public class Main {
     }
     public static void task1 () {
         System.out.println("Задача 1");
-        for ( int i = 1; i <= 10; i = i + 1) {
+        for ( int i = 1; i <= 10; i++) {
             System.out.println("Число " + i);
         }
     }
     public static void task2 () {
         System.out.println("Задача 2");
-        for ( int i = 10; i <= 10 && i >= 1; i = i - 1) {
+        for ( int i = 10; i >= 1; i--) {
             System.out.println("Число " + i);
         }
     }
     public static void task3 () {
         System.out.println("Задача 3");
-        for ( int i = 2; i < 17; i = i + 2) {
+        for ( int i = 0; i < 17; i += 2) {
             System.out.println("Четное число " + i);
         }
     }
     public static void task4 () {
         System.out.println("Задача 4");
-        for ( int i = 10; i <= 10 && i >= -10; i = i - 1) {
+        for ( int i = 10; i >= -10; i--) {
             System.out.println("Число " + i);
         }
     }
     public static void task5 () {
         System.out.println("Задача 5");
-        for ( int i = 1904; i < 2097; i = i + 4) {
-            System.out.println( i + " год является высокосным");
+        for ( int i = 1904; i < 2097; i++) {
+            if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0) {
+                System.out.println(i + " год является высокосным");
+            }
         }
     }
     public static void task6 () {
         System.out.println("Задача 6");
-        for ( int i = 7; i < 99; i = i + 7) {
-            System.out.println( i );
+        for ( int i = 7; i < 99; i += 7) {
+            System.out.print( i + " ");
         }
+        System.out.println();
     }
     public static void task7 () {
         System.out.println("Задача 7");
         for ( int i = 1; i < 513; i = i * 2) {
-            System.out.println( i );
+            System.out.print(i + " ");
         }
+        System.out.println();
     }
     public static void task8 () {
         System.out.println("Задача 8");
         int stash = 29000;
-        float total = 0f;
+        int total = 0;
         for ( int i = 1; i <= 12; i++ ) {
-            total = total + stash;
+            total += stash;
             System.out.println("Месяц " + i + " cумма накоплений равна " + total + " рублей");
         }
     }
@@ -67,6 +71,8 @@ public class Main {
         int stash = 29000;
         float total = 0f;
         for ( int i = 1; i <= 12; i++) {
+            /*Непонятно в какой момент должны начисляться проценты,
+             поэтому строки в теле цикла можно поменять местами*/
             total = total + total/100;
             total = total + stash;
             System.out.println("Месяц " + i + " cумма накоплений равна " + total + " рублей");
