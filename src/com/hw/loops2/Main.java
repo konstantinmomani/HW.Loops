59c0cd333256f3b79098e5b009f0ef16c0c66e07
konstantin <mom.konstantin@gmail.com>
2023-01-24 17:07:04 +0300

    Циклы2 Задача 1

--- src/com/hw/loops2/Main.java
+++ src/com/hw/loops2/Main.java
@@ -1,10 +1,19 @@
 package com.hw.loops2;
 
 public class Main {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         task1();
     }
-    public static void task1 () {
+
+    public static void task1() {
         System.out.println("Задача 1");
+        int stash = 15000;
+        int total = 0;
+        int month = 0;
+        while (total < 2_459_000) {
+            month +=1;
+            total += stash;
+            System.out.println("Месяц " + month + " cумма накоплений равна " + total + " рублей");
+        }
     }
 }
