cf018aac8d8dae2d17f05d92b66561e2ff95f527
konstantin <mom.konstantin@gmail.com>
2023-01-25 00:18:04 +0300

    Циклы2 Задача 1-8

--- src/com/hw/loops2/Main.java
+++ src/com/hw/loops2/Main.java
@@ -9,6 +9,7 @@ public class Main {
         task5();
         task6();
         task7();
+        task8();
     }
 
     public static void task1() {
@@ -17,16 +18,17 @@ public class Main {
         int total = 0;
         int month = 0;
         while (total < 2_459_000) {
-            month +=1;
+            month += 1;
             total += stash;
             System.out.println("Месяц " + month + " cумма накоплений равна " + total + " рублей");
         }
     }
-    public static void task2 () {
+
+    public static void task2() {
         System.out.println("Задача 2");
         int i = 0;
         while (i < 10) {
-            i +=1;
+            i += 1;
             System.out.print(i + " ");
         }
         System.out.println();
@@ -35,7 +37,8 @@ public class Main {
         }
         System.out.println();
     }
-    public static void task3 () {
+
+    public static void task3() {
         System.out.println("Задача 3");
         int populationY = 12_000_000;
         int birthRate = 17 * populationY / 1000;
@@ -48,6 +51,7 @@ public class Main {
             System.out.println("Год " + year + ", население составляет " + populationY);
         }
     }
+
     public static void task4() {
         System.out.println("Задача 4");
         int stash = 15_000;
@@ -58,9 +62,10 @@ public class Main {
              поэтому строки в теле цикла можно поменять местами*/
             total += total * 0.07;
             total += stash;
-                System.out.println("Месяц " + month + " cумма накоплений равна " + total + " рублей");
-            }
+            System.out.println("Месяц " + month + " cумма накоплений равна " + total + " рублей");
         }
+    }
+
     public static void task5() {
         System.out.println("Задача 5");
         int stash = 15_000;
@@ -76,6 +81,7 @@ public class Main {
             }
         }
     }
+
     public static void task6() {
         System.out.println("Задача 6");
         int stash = 15_000;
@@ -91,7 +97,8 @@ public class Main {
             }
         }
     }
-    public static void task7 () {
+
+    public static void task7() {
         System.out.println("Задача 7");
         int day = 1;
         int friday = 6;
@@ -99,6 +106,18 @@ public class Main {
             if (day == friday) {
                 System.out.println("Сегодня пятница, " + day + "-е число");
                 friday += 7;
+            }
+        }
+    }
+
+    public static void task8() {
+        System.out.println("Задача 8");
+        int presentYear = 2023;
+        int past200Years = presentYear - 200;
+        int next100Years = presentYear + 100;
+        for (int i = 0; i <= next100Years; i++) {
+            if (i % 79 == 0 && i >= past200Years) {
+                System.out.println(i);
             }
         }
     }
